a2cb5be39645296ae0856d2f08328ac52fa0c90f
Naveenkumar506 <naveennani0603@gmail.com>
2025-09-13 11:55:20 +0530

    des

--- MapFilterReduce & ES6/99destructuring.js
+++ MapFilterReduce & ES6/99destructuring.js
@@ -13,9 +13,29 @@ console.log(uname);
 console.log(uage);
 console.log(uloc);
 
-//3Providing Default Values
+//3.Providing Default Values
 
 let studentinfo = { sid: 1001, sname: "jaya" };
 let { sid, sname, sMobileno = 1234567890 } = studentinfo;
 console.log(studentinfo);
 console.log(sMobileno);
+
+//4.Nested Object Destructuring
+let employee = {
+  Ename: "saravana",
+  Eaddress: {
+    ELoc: "Hyd",
+    MobileNo: 1234567890,
+  },
+};
+console.log(employee);
+console.log(employee.Eaddress.MobileNo);
+
+//
+
+function cars({ name, price }) {
+  //   console.log(`car_NAME:- ${name}, car_PRICE:- ${price}`);
+  console.log("car_NAME:- " + name + ", car_PRICE:- " + price);
+}
+const newcars = { name: "Bmw", price: "200000" };
+cars(newcars);
